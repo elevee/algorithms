fbe3324181a07be523648b0e2c6eb94a3b6f5b63
Eric Levine <elevine@getty.edu>
2019-03-05 12:08:21 -0800

    Added alternative solution

--- other/fizzbuzz.js
+++ other/fizzbuzz.js
@@ -15,8 +15,13 @@ for(let i=1; i<101; i++){
     console.log(output);
 }
 
-// Simplification: make the modulo 15
-
+// Simplification: make the modulo 15, use else if structure
+// for (var i=1; i < 101; i++){
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
 
 
 // Shortest solution
