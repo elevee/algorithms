a63f05e6313483408c0c58e07539bbd80917286d
Eric Levine <elevine@getty.edu>
2019-03-14 12:31:43 -0700

    Completed digit degree

--- 100_algos_challenge/digitDegree/digitDegree.ts
+++ 100_algos_challenge/digitDegree/digitDegree.ts
@@ -1,7 +1,13 @@
 function digitDegree(n: number): number {
-  
+   let result = n.toString(), //convert to string to check length of digits
+       counter = 0;
+    // console.log(result.length !== 1);
+   while(result.length !== 1){
+     result = result.split('').reduce(function(acc, cur) { return parseInt(acc) + parseInt(cur) }, 0).toString();
+     counter++;
+   }
+   return counter;
 }
-
 console.log(digitDegree(5));
 console.log(digitDegree(10));
 console.log(digitDegree(91));
