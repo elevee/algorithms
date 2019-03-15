7b4caeeb89b840d1636c2f5f2e7668779e070f8b
Eric Levine <elevine@getty.edu>
2019-03-14 22:32:18 -0700

    Solved addBorder

--- 100_algos_challenge/addBorder/addBorder.ts
+++ 100_algos_challenge/addBorder/addBorder.ts
@@ -1,5 +1,16 @@
 function addBorder(picture: string[]): string[] {
-
+    const frame = "*",
+          output = [],
+          picWidth = picture[0].length,
+          totalWidth = picWidth + 2;
+    
+    output.push(frame.repeat(totalWidth));
+    for(row of picture){
+        output.push(frame.concat(row,frame));
+    }
+    output.push(frame.repeat(totalWidth));
+    return output;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abcde", "deded"]));
