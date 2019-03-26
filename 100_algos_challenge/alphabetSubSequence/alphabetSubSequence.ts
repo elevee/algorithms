f7015275a7fdc2bdfa4ee1cbdb1cd52eb4f6d57e
Eric Levine <elevine@getty.edu>
2019-03-26 15:58:17 -0700

    Finished alphabetic shift & alphabet subsequence

--- 100_algos_challenge/alphabetSubSequence/alphabetSubSequence.ts
+++ 100_algos_challenge/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,54 @@
-function alphabetSubsequence(s: string): boolean {
-
+//for my sanity
+function range(begin, end) {
+  output = []; //if we want an array
+  //   let obj_index = 0;
+  //   output = {};
+  for (let i = begin.charCodeAt(); i <= end.charCodeAt(); i++) {
+    output.push(String.fromCharCode(i)); //if we want an array
+    // output[obj_index] = String.fromCharCode(i);
+    // obj_index++;
+  }
+  return output;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+// console.log(range("a", "z"));
+
+//FIRST GO
+// function alphabetSubsequence(s: string): boolean {
+//   let alphabet = range("a", "z");
+//   let position = -1;
+//   for (const char of s) {
+//     //loop through str. Don't need indices
+//     // console.log(`index of ${char} is ${alphabet.indexOf(char)}`);
+//     let newPos = alphabet.indexOf(char); //find the position the letter is in
+//     if (newPos <= position) return false; //make sure it's greater than our current position (this handles duplicates)
+//     position = newPos; //set new position to the most recent
+//   }
+//   return true;
+// }
+
+/*  
+    IF we wanna do this without an alphabet, we can do so by just going
+    by character codes. They are in order and as long as they are not less than 
+    or equal to the previous one, it's valid.
+*/
+function alphabetSubsequence(s: string): boolean {
+  let charCodes = s.split("").map(char => char.charCodeAt(0)); //creating array of charCodes
+  let set = new Set(charCodes); // push each char into set. if lengths don't match up, we have duplicates (Set items are unique)
+
+  if (set.size !== s.length) {
+    //if Set enforces uniqueness, it's got duplicates.
+    return false;
+  } else {
+    for (let i = 0; i < charCodes.length - 1; i++) {
+      if (charCodes[i] > charCodes[i + 1]) return false; // if current charCode is greater than the next one, that ain't right!
+    }
+  }
+  return true;
+}
+
+console.log(alphabetSubsequence("zab")); //false
+console.log(alphabetSubsequence("effg")); //false
+console.log(alphabetSubsequence("cdce")); //false
+console.log(alphabetSubsequence("ace")); //true
+console.log(alphabetSubsequence("bxz")); //true
--- 100_algos_challenge/alphabeticShift/alphabeticShift.ts
+++ 100_algos_challenge/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,93 @@
-function alphabeticShift(inputString: string): string {
+//FIRST GO. Feels brutal.
+// function alphabeticShift(inputString: string): string {
+//   let output = "";
+//   let alphaArray = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z"
+//   ];
+//   for (let i = 0; i < inputString.length; i++) {
+//     const char = inputString[i];
+//     console.log(alphaArray[i + 1]);
+//     let nextIndex = alphaArray.indexOf(char) + 1;
+//     output += alphaArray[nextIndex] ? alphaArray[nextIndex] : alphaArray[0];
+//   }
+//   return output;
+// }
 
+//Second Try. Map function. Made alphaArray instantiation less miserable
+// function alphabeticShift(inputString: string): string {
+//   let output = "";
+//   let alphaArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+//   ];
+
+//   return inputString
+//     .split("")
+//     .map(char => {
+//       let nextIndex = alphaArray.indexOf(char) + 1;
+//       return alphaArray[nextIndex] ? alphaArray[nextIndex] : alphaArray[0];
+//     })
+//     .join("");
+// }
+
+//Another faster solution, but more tedious
+function alphabeticShift(inputString: string): string {
+  let output = "";
+  let alphaObj = {
+    a: "b",
+    b: "c",
+    c: "d",
+    d: "e",
+    e: "f",
+    f: "g",
+    g: "h",
+    h: "i",
+    i: "j",
+    j: "k",
+    k: "l",
+    l: "m",
+    m: "n",
+    n: "o",
+    o: "p",
+    p: "q",
+    q: "r",
+    r: "s",
+    s: "t",
+    t: "u",
+    u: "v",
+    v: "w",
+    w: "x",
+    x: "y",
+    y: "z",
+    z: "a"
+  };
+  for (let i = 0; i < inputString.length; i++) {
+    output += alphaObj[inputString[i]];
+  }
+  return output;
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));
