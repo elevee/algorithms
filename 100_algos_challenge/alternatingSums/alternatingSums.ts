71a8480d1c536630bb793579c5626aaf4c1ad611
Eric Levine <elevine@getty.edu>
2019-03-27 10:22:05 -0700

    found o(n) solution to alternatingSums

--- 100_algos_challenge/alternatingSums/alternatingSums.ts
+++ 100_algos_challenge/alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
+// O(n) time, O(1) space
 function alternatingSums(a: number[]): number[] {
-
+  //loop through weights
+  const firstTotal = 0,
+    secondTotal = 0; //create two variables, firstTotal and secondTotal, set to 0.
+  for (let i = 0; i < a.length; i++) {
+    const weight = a[i];
+    i % 2 === 0 ? (firstTotal += weight) : (secondTotal += weight); //if index is even, add it to first, else, second.
+  }
+  return [firstTotal, secondTotal];
+  //return an array of two integers
+  //first el - total weight of team1
+  //second el - total weight of team2
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
