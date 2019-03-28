86bdb0007bcbf851b78698e4658291f783ded7e8
Eric Levine <elevine@getty.edu>
2019-03-28 10:55:24 -0700

    Added solution to this. Weird wording on question

--- 100_algos_challenge/areEquallyStrong/areEquallyStrong.ts
+++ 100_algos_challenge/areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,21 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+// this is the solution, although the wording of the question was so
+// confusing that it was tough to glean what they were asking.
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest =
+    friendsLeft >= friendsRight ? friendsLeft : friendsRight;
 
+  return yourWeakest === friendsWeakest && yourStrongest === friendsStrongest;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
