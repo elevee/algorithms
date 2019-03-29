263dcc88b8a51d2cc7e7ac62053cbeca94ef82a5
Eric Levine <elevine@getty.edu>
2019-03-29 07:01:49 -0700

    Finished arrayPreviousLess

--- 100_algos_challenge/arrayPreviousLess/arrayPreviousLess.ts
+++ 100_algos_challenge/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,21 @@
+// FIRST GO: I hate to do nested loops but here we go
 function arrayPreviousLess(items: number[]): number[] {
-
+  for (let i = items.length - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      //loop again from the previous element toward the beginning finding the first
+      //value that satisfies the less-than comparison.
+      if (items[j] < items[i]) {
+        items.splice(i, 1, items[j]);
+        break; //we're done looking for element i
+      }
+      items.splice(i, 1, -1);
+    }
+    if (i === 0) {
+      //out of bounds edge case where we never enter nested loop because we're on the first el
+      items.splice(i, 1, -1);
+    }
+  }
+  return items; // return modified array
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
