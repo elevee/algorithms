b83c61de8f74ff57889fd9ed6c334a4932238a87
Eric Levine <elevine@getty.edu>
2019-03-29 07:22:22 -0700

    Easy buckets!

--- 100_algos_challenge/arrayReplace/arrayReplace.ts
+++ 100_algos_challenge/arrayReplace/arrayReplace.ts
@@ -1,5 +1,19 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+// O(n)
+function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  //loop through inputArray
+  //if value === elemtoReplace, replace it with substitutionElem
+  //return modified array
+
+  inputArray.forEach((elem, i) => {
+    if (elem === elemToReplace) {
+      inputArray.splice(i, 1, substitutionElem);
+    }
+  });
+  return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
